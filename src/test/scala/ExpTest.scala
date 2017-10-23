10268f9baf60fe638d326faf9d2e520417f8178b
Tobias Kerst <tobias.kerst@tobscore.com>
2017-10-23 19:08:19 +0200

    Tests the sugar candy way of writing expressions

--- src/test/scala/ExpTest.scala
+++ src/test/scala/ExpTest.scala
@@ -9,24 +9,55 @@ class ExpTest extends FunSuite {
   val e4 = Divide(Val(Fraction(5, 6)), Minus(e1, Val(Fraction(2, 4))))
   val e5 = Multiply(Val(Fraction(10, 13)), Val(Fraction(12, 14)))
 
+  val e1_short = Val(1, 2)
+  val e2_short = Val(3, 4) + e1
+  val e3_short = ((Val(3, 4) + Val(1, 2)) * Val(1, 2)) - (Val(10, 13) / (Val(3, 4) + Val(1, 2)))
+  val e4_short = Val(5, 6) / (Val(1, 2) - Val(2, 4))
+  val e5_short = Val(10, 13) * Val(12, 14)
+
+  val e2_pretty = "((3/4)+(1/2))"
+  val e3_pretty = "((((3/4)+(1/2))*(1/2))-((10/13)//((3/4)+(1/2))))"
+  val e4_pretty = "((5/6)//((1/2)-(2/4)))"
+  val e5_pretty = "((10/13)*(12/14))"
+
   test("Prettify e1") {
     assert(ExpParser.pretty(e1) == "(1/2)")
   }
 
+  test("Prettify e1 short") {
+    assert(ExpParser.pretty(e1_short) == "(1/2)")
+  }
+
   test("Prettify e2") {
-    assert(ExpParser.pretty(e2) == "((3/4)+(1/2))")
+    assert(ExpParser.pretty(e2) == e2_pretty)
+  }
+
+  test("Prettify e2 short") {
+    assert(ExpParser.pretty(e2_short) == e2_pretty)
   }
 
   test("Prettify e3") {
-    assert(ExpParser.pretty(e3) == "((((3/4)+(1/2))*(1/2))-((10/13)//((3/4)+(1/2))))")
+    assert(ExpParser.pretty(e3) == e3_pretty)
+  }
+
+  test("Prettify e3 short") {
+    assert(ExpParser.pretty(e3_short) == e3_pretty)
   }
 
   test("Prettify e4") {
-    assert(ExpParser.pretty(e4) == "((5/6)//((1/2)-(2/4)))")
+    assert(ExpParser.pretty(e4) == e4_pretty)
+  }
+
+  test("Prettify e4 short") {
+    assert(ExpParser.pretty(e4_short) == e4_pretty)
   }
 
   test("Prettify e5") {
-    assert(ExpParser.pretty(e5) == "((10/13)*(12/14))")
+    assert(ExpParser.pretty(e5) == e5_pretty)
+  }
+
+  test("Prettify e5 short") {
+    assert(ExpParser.pretty(e5_short) == e5_pretty)
   }
 
   test("Simplify 2/4") {
@@ -125,19 +156,19 @@ class ExpTest extends FunSuite {
   }
 
   test("Eval (1/2) * (3,2) + (5,4) / (7, 3)") {
-    val exp = Plus(Multiply(Val(Fraction(1,2)), Val(Fraction(3,2))), Divide(Val(Fraction(5, 4)), Val(Fraction(7,3))))
+    val exp = Plus(Multiply(Val(Fraction(1, 2)), Val(Fraction(3, 2))), Divide(Val(Fraction(5, 4)), Val(Fraction(7, 3))))
     assert(ExpParser.eval(exp).get == Fraction(9, 7))
   }
 
   test("Adding two Vals by calling + method") {
-    val exp = Val(Fraction(2,3)) + Val(Fraction(1,3))
-    assert(exp == Plus(Val(Fraction(2,3)), Val(Fraction(1,3))))
+    val exp = Val(Fraction(2, 3)) + Val(Fraction(1, 3))
+    assert(exp == Plus(Val(Fraction(2, 3)), Val(Fraction(1, 3))))
   }
 
   test("Simplify writing an expression") {
-    val exp = Val(2,3) + Val(1,3)
-    assert(exp == Plus(Val(Fraction(2,3)), Val(Fraction(1,3))))
-    assert(ExpParser.eval(exp).get == Fraction(1,1))
+    val exp = Val(2, 3) + Val(1, 3)
+    assert(exp == Plus(Val(Fraction(2, 3)), Val(Fraction(1, 3))))
+    assert(ExpParser.eval(exp).get == Fraction(1, 1))
   }
 
   test("Eval e3") {
